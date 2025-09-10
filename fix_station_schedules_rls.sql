ba7b289e04b50c803e67f0aebf3c981402ecf27f
ray73 <serviceplanning@watercharging.com>
2025-09-10 20:12:12 +0900

    fix: comprehensive drop of all station_schedules policies before recreating

--- fix_station_schedules_rls.sql
+++ fix_station_schedules_rls.sql
@@ -1,9 +1,14 @@
 -- station_schedules RLS 정책 수정
 
--- 기존 정책 삭제
+-- 기존 정책들 모두 삭제
 DROP POLICY IF EXISTS "station_sched_select_all" ON public.station_schedules;
 DROP POLICY IF EXISTS "station_sched_upsert_auth" ON public.station_schedules;
 DROP POLICY IF EXISTS "station_sched_service_all" ON public.station_schedules;
+DROP POLICY IF EXISTS "station_schedules_select_all" ON public.station_schedules;
+DROP POLICY IF EXISTS "station_schedules_insert_auth" ON public.station_schedules;
+DROP POLICY IF EXISTS "station_schedules_update_auth" ON public.station_schedules;
+DROP POLICY IF EXISTS "station_schedules_delete_auth" ON public.station_schedules;
+DROP POLICY IF EXISTS "station_schedules_service_role_all" ON public.station_schedules;
 
 -- 새로운 정책 생성
 CREATE POLICY "station_schedules_select_all" ON public.station_schedules
